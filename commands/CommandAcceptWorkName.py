d643cede5319fc1e2f3445b2649b2f9af8cd6d00
n1ck <qserxsdiv@gmail.com>
2018-01-25 23:42:02 +0700

    fix

--- commands/CommandAcceptWorkName.py
+++ commands/CommandAcceptWorkName.py
@@ -13,19 +13,18 @@ class CommandAcceptWorkName(Command):
         works_collection = database[os.environ.get('MONGO_COLLECTION_WORKS')]
         # users_collection = database[os.environ.get('MONGO_COLLECTION_USERS')]
 
-        work = works_collection.find_one_and_update({"_id": arguments[0]},
-                                                    {"$set": {"address": message['text']}})
+        work = works_collection.find_one_and_update({'_id': arguments[0]},
+                                                    {'$set': {'address': message['text']}})
 
-        response = {'chat_id': message['chat']['id'], "text": "Адрес работы ({}) задан".format(work)}
-        response['debug'] = arguments
+        response = {'chat_id': message['chat']['id'], 'text': 'Адрес работы ({}) задан'.format(work)}
         works = list(works_collection.find({}))
 
         keyboard = {'inline_keyboard': [
-            [{"text": "Добавить объект", "callback_data": "create_work"}]
+            [{'text': 'Добавить объект', 'callback_data': 'create_work'}]
         ]}
         for work in works:
-            keyboard['inline_keyboard'].append([{"text": work['address'],
-                                                 "callback_data": "edit_work:{}".format(work['_id'])}
+            keyboard['inline_keyboard'].append([{'text': work['address'],
+                                                 'callback_data': 'edit_work:{}'.format(work['_id'])}
                                                 ])
         response['reply_markup'] = json.dumps(keyboard)
 
