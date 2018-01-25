8b405d8179113d9308bc930681f8a6434590b562
n1ck <qserxsdiv@gmail.com>
2018-01-25 23:15:59 +0700

    fix

--- commands/CommandAcceptWorkName.py
+++ commands/CommandAcceptWorkName.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from commands.Command import Command
@@ -10,22 +11,21 @@ class CommandAcceptWorkName(Command):
     def __call__(self, arguments: list, message: dict) -> dict:
         database = self.client[os.environ.get('MONGO_DBNAME')]
         works_collection = database[os.environ.get('MONGO_COLLECTION_WORKS')]
-        users_collection = database[os.environ.get('MONGO_COLLECTION_USERS')]
+        # users_collection = database[os.environ.get('MONGO_COLLECTION_USERS')]
 
         work = works_collection.find_one_and_update({"_id": arguments[0]},
                                                     {"$set": {"address": message['text']}})
-        users_collection.find_one_and_update({'username': message['chat']['username']},
-                                             {"$set": {'command': 'accept_photo:{}'.format(work['_id'])}})
+
         response = {'chat_id': message['chat']['id'], "text": "Адрес работы задан"}
         works = list(works_collection.find({}))
 
-        # keyboard = {'inline_keyboard': [
-        #     [{"text": "Добавить объект", "callback_data": "create_work"}]
-        # ]}
-        # for work in works:
-        #     keyboard['inline_keyboard'].append([{"text": work['address'],
-        #                                          "callback_data": "edit_work:{}".format(work['_id'])}
-        #                                         ])
-        # response['reply_markup'] = json.dumps(keyboard)
+        keyboard = {'inline_keyboard': [
+            [{"text": "Добавить объект", "callback_data": "create_work"}]
+        ]}
+        for work in works:
+            keyboard['inline_keyboard'].append([{"text": work['address'],
+                                                 "callback_data": "edit_work:{}".format(work['_id'])}
+                                                ])
+        response['reply_markup'] = json.dumps(keyboard)
 
         return response
