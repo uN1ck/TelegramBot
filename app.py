c169758b5255a1bfcda9bec6568024fefe6fe743
n1ck <qserxsdiv@gmail.com>
2018-01-26 22:00:51 +0700

    fix

--- app.py
+++ app.py
@@ -99,21 +99,22 @@ def webhook_handler():
                 send_reply(response)
             else:
                 message = update['message']
-                text = message['text']
-                if text[0] == '/':
-                    command, *arguments = text.split(" ", 1)
-                    response = PUBLIC_CMD.get(command, PRIVATE_CMD['default'])(arguments, message)
-                    send_reply(response)
-                else:
-                    database = CLIENT[os.environ.get('MONGO_DBNAME')]
-                    users_collection = database[os.environ.get('MONGO_COLLECTION_USERS')]
-                    command = users_collection.find_one({'username': message['chat']['username']})['command']
-                    if ':' in command:
-                        command = command.split(':')
-                    print("@COMMAND: {}".format(command))
-                    response = CMD.get(command[0], PRIVATE_CMD['default'])(command[1:], message)
-                    print("@RESPONSE: {}".format(response))
-                    send_reply(response)
+                if 'text' in message:
+                    text = message['text']
+                    if text[0] == '/':
+                        command, *arguments = text.split(" ", 1)
+                        response = PUBLIC_CMD.get(command, PRIVATE_CMD['default'])(arguments, message)
+                        send_reply(response)
+                    else:
+                        database = CLIENT[os.environ.get('MONGO_DBNAME')]
+                        users_collection = database[os.environ.get('MONGO_COLLECTION_USERS')]
+                        command = users_collection.find_one({'username': message['chat']['username']})['command']
+                        if ':' in command:
+                            command = command.split(':')
+                        print("@COMMAND: {}".format(command))
+                        response = CMD.get(command[0], PRIVATE_CMD['default'])(command[1:], message)
+                        print("@RESPONSE: {}".format(response))
+                        send_reply(response)
                 # elif 'photo' in message:
                 #     response = CommandAcceptPhoto(CLIENT, API)([], message)
                 #     send_reply(response)
