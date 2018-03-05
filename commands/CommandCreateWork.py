6b81dd421263f7d47ba40c2fb45799ee46ebcacb
n1ck <qserxsdiv@gmail.com>
2018-03-05 10:24:00 +0700

    fix

--- commands/CommandCreateWork.py
+++ commands/CommandCreateWork.py
@@ -13,7 +13,7 @@ class CommandCreateWork(Command):
 
         users_collection.find_one_and_update(
             {'username': message['chat']['username']},
-            {"$set": {'command': 'accept_work_name:0'}})
+            {"$set": {'command': 'accept_work_name'}})
         response = {
             'chat_id': message['chat']['id'],
             'text': 'Задайте адрес для работы: ',
