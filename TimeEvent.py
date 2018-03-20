f4507186b9a8ec44111c01b3b237b075b528486f
n1ck <qserxsdiv@gmail.com>
2018-03-20 09:47:19 +0700

    time event commit

---
+++ TimeEvent.py
@@ -0,0 +1,17 @@
+import os
+
+from app import CLIENT, API
+
+
+def notify():
+    database = CLIENT[os.environ.get('MONGO_DBNAME')]
+    users_collection = database[os.environ.get('MONGO_COLLECTION_USERS')]
+
+    users = list(users_collection.find({}))
+
+    for item in users:
+        response = {
+            'chat_id': item["chat_id"],
+            'text': "Не забудьте выставить фотографии!"
+        }
+        API.post(os.environ.get('URL') + "sendMessage", data=response)
