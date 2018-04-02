56956d9463561c9a847c3d0107ca21b3c4a71bbe
n1ck <qserxsdiv@gmail.com>
2018-04-02 23:06:36 +0700

    Fix

--- commands/CommandAcceptPhoto.py
+++ commands/CommandAcceptPhoto.py
@@ -57,7 +57,7 @@ class CommandAcceptPhoto(Command):
             works_collection = database[os.environ.get('MONGO_COLLECTION_WORKS')]
 
             works_collection.find_one_and_update({"_id": ObjectId(arguments[0])},
-                                                 {'$push': {"{}|{}".format(str(datetime.now()), file['file_id'])}})
+                                                 {'$push': {"{}|{}".format(str(datetime.now().date()), file['file_id'])}})
 
             yd = yadisk.YaDisk(os.environ.get('YA_ID'), os.environ.get('YA_SECRET'), os.environ.get('YA_TOKEN'))
             if not yd.exists('/{}'.format(work_name)):
