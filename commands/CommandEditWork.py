b285d9d42782a9bc34146ab3f42f839175049084
n1ck <qserxsdiv@gmail.com>
2018-04-03 22:25:58 +0700

    Fix

--- commands/CommandEditWork.py
+++ commands/CommandEditWork.py
@@ -27,13 +27,12 @@ class CommandEditWork(Command):
             response['debug'] = keyboard
             try:
                 keyboard['inline_keyboard'] = [
+                    [[{"text": item.strftime('%Y-%m-%d'),
+                       "callback_data": "return_photo_by_date:{}:{}".format(item.strftime('%Y-%m-%d'), arguments[0])}]
+                     for item in works_set],
                     [{"text": "Отчет", "callback_data": "get_work_report:{}".format(work_id)}],
                     [{"text": "Удалить", "callback_data": "delete_work:{}".format(work_id)}]
                 ]
-                works_button = [
-                    [{"text": item.strftime('%Y-%m-%d'),
-                      "callback_data": "return_photo_by_date:{}:{}".format(item.strftime('%Y-%m-%d'), arguments[0])}]
-                    for item in works_set]
 
                 response['reply_markup'] = json.dumps(keyboard)
             except Exception as ex:
